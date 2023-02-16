00375cb92db1c18331883af01eaa523beafd8478
Gabe Amaya <gabeamaya2@gmail.com>
2023-02-16 11:45:01 -0600

    refactored dex

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { reactive, ref, computed, onMounted } from 'vue'
-import PokeCard from '../components/PokeCard.vue';
 
 const pokemonStore = reactive({
   list: [],
@@ -39,11 +38,18 @@ onMounted( async () => {
     <h1 class="text-3xl font-bold mb-4">Gabe's Pokedex</h1>
     <div class="flex flex-col">
       <label for="search-pokemon" class="block mb-2 text-sm">Search Pokemon</label>
-      <input type="text" v-model="filterText" class="block w-full text-gray-600 rounded-sm p-2 focus:ring-emerald-600 focus:border-emerald-600">
+      <input type="text" v-model="filterText" class="block w-full text-gray-600 rounded-sm p-2 focus:ring-emerald-600 focus:border-emerald-600 mb-8">
       <!-- <button class="block text-white py-2 px-4 rounded-sm bg-violet-600 mt-4 font-bold">Search Pokemon</button> -->
     </div>
-    <ul>
-        <PokeCard v-for="(pokemon, index) in pokemonStore.filteredList" :key="`poke-${index}`" :number="pokemon.entry_number" :name="pokemon.pokemon_species.name" />
-    </ul>
+    <article v-for="(pokemon, index) in pokemonStore.filteredList" :key="`poke-${index}`" class="flex flex-col mt-4 justify-center p-6 bg-zinc-300 rounded-sm shadow-sm">
+      <p>{{ pokemon.type.name }}</p>
+      <p class="text-lg text-stone-700">#{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }}</p>
+    </article>
+
+    <!-- <ul>
+      <li v-for="(pokemon, index) in pokemonStore.filteredList" :key="`poke-${index}`">#{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }}</li>
+      <PokeCard v-for="(pokemon, index) in pokemonStore.filteredList" :key="`poke-${index}`" :number="pokemon.entry_number" :name="pokemon.pokemon_species.name"/>
+    </ul> -->
+
   </main>
 </template>
